101c07e9eaf04982f457de3206026b0217d21d25
suyuwen1 <suyuwen1@126.com>
2014-11-19 17:56:50 +0800

    2014.11.19

---
+++ ihref_gg/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>爱分享广告</title>
+<script src="js/jquery-2.1.1.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="123">
+<script src="js/c.js" type="text/javascript"></script>
+</div>
+</body>
+</html>
---
+++ ihref_gg/js/c.js
@@ -0,0 +1,5 @@
+$(function(){
+	document.write('<div id="ihref_gg1"></div>');
+	var i=$("#ihref_gg1").parent("div").attr("id");
+	alert(i);
+})
