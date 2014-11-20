04c5e9ee43836228b5863643c38d500ffe435ada
suyuwen1 <suyuwen1@126.com>
2014-11-20 17:38:10 +0800

    2014.11.20

--- ihref_gg/index.php
+++ ihref_gg/index.php
@@ -4,11 +4,16 @@
 <meta charset="utf-8">
 <title>爱分享广告</title>
 <script src="js/jquery-2.1.1.min.js" type="text/javascript"></script>
+<link href="css/ihref_gg.css" type="text/css" rel="stylesheet">
+<style>
+</style>
 </head>
 
 <body>
 <div id="123">
 <script src="js/c.js" type="text/javascript"></script>
+</div>fasdfa
 </div>
+afsdfafasdf
 </body>
 </html>
--- ihref_gg/js/c.js
+++ ihref_gg/js/c.js
@@ -1,5 +1,9 @@
+$("head").append('<link href="css/ihref_gg.css" rel="stylesheet" type="text/css">');
+
+document.write('<div id="ihref_gg1"><iframe frameborder="0" scrolling="no" src="http://www.baidu.com" style="z-index: 99991; width: 100%; height: 500px; border: 0px;"></iframe><a id="ihref_close" href="javacript:void(0)">关闭</a></div>');
+
 $(function(){
-	document.write('<div id="ihref_gg1"></div>');
-	var i=$("#ihref_gg1").parent("div").attr("id");
-	alert(i);
+	$("#ihref_close").click(function(){
+		$("#ihref_gg1").stop().slideUp("fast");
+	});
 })
