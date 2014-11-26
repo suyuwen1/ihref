c079549b67ca18e977752e2f382f555e01691861
suyuwen1 <suyuwen1@126.com>
2014-11-26 10:52:40 +0800

    2014.11.26

--- ihref_gg/index.php
+++ ihref_gg/index.php
@@ -23,20 +23,20 @@
 <div id="main">
 	<div id="top">
 		<ul>
-			<li class="img img1" id="img1"><a href="#"><img src="img/1.jpg" alt=""></a></li>
-			<li class="img" id="img2"><a href="#"><img src="img/2.jpg" alt=""></a></li>
-			<li class="img" id="img3"><a href="#"><img src="img/3.jpg" alt=""></a></li>
-			<li class="img" id="img4"><a href="#"><img src="img/1.jpg" alt=""></a></li>
-			<li class="img" id="img5"><a href="#"><img src="img/1.jpg" alt=""></a></li>
+			<li class="img img1" id="img1"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DnaH%252FCrshUsEjmraEDZVrLofrhNijlTODokW4bPA0wMWLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HyODyAjanCszm3abJM7sDg2L3Kj9Edr7oKJjU1rGI%252FCdg%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28024044"><img src="img/1.jpg" alt="移动电源"></a></li>
+			<li class="img" id="img2"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185"><img src="img/2.jpg" alt="小米手机4"></a></li>
+			<li class="img" id="img3"><a target="_blank" href="#"><img src="img/3.jpg" alt=""></a></li>
+			<li class="img" id="img4"><a target="_blank" href="#"><img src="img/1.jpg" alt=""></a></li>
+			<li class="img" id="img5"><a target="_blank" href="#"><img src="img/1.jpg" alt=""></a></li>
 		</ul>
 	</div>
 	<div id="down">
 		<ul>
-			<li class="list list1" i="1"><span class="bt"><a href="#">移动电源</a></span><span class="jg">¥ 130.9</span></li>
-			<li class="list" i="2"><span class="bt"><a href="#">Iphone 6 plus</a></span><span class="jg">¥ 130</span></li>
-			<li class="list" i="3"><span class="bt"><a href="#">移动电源</a></span><span class="jg">¥ 9.9</span></li>
-			<li class="list" i="4"><span class="bt"><a href="#">Iphone 6 plus</a></span><span class="jg">¥ 11</span></li>
-			<li class="list" i="5"><span class="bt"><a href="#">移动电源</a></span><span class="jg">¥ 90.1</span></li>
+			<li class="list list1" i="1"><span class="bt"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DnaH%252FCrshUsEjmraEDZVrLofrhNijlTODokW4bPA0wMWLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HyODyAjanCszm3abJM7sDg2L3Kj9Edr7oKJjU1rGI%252FCdg%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28024044">移动电源</a></span><span class="jg">¥ 49.9</span></li>
+			<li class="list" i="2"><span class="bt"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185">小米手机4</a></span><span class="jg">¥ 2098</span></li>
+			<li class="list" i="3"><span class="bt"><a target="_blank" href="#">移动电源</a></span><span class="jg">¥ 9.9</span></li>
+			<li class="list" i="4"><span class="bt"><a target="_blank" href="#">Iphone 6 plus</a></span><span class="jg">¥ 11</span></li>
+			<li class="list" i="5"><span class="bt"><a target="_blank" href="#">移动电源</a></span><span class="jg">¥ 90.1</span></li>
 		</ul>
 	</div>
 </div>
