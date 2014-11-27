9c69ac1e1395c4deb42e07d4a2f65aef98b97e49
suyuwen1 <suyuwen1@126.com>
2014-11-27 11:48:10 +0800

    2014.11.27

--- ihref_gg/index.php
+++ ihref_gg/index.php
@@ -1,3 +1,6 @@
+<?php 
+	$ys = array('#60c2e7' , '#222222' , '#fff' , '#ccc' , '#fff');
+?>
 <html>
 <head>
 <meta charset="utf-8">
@@ -8,7 +11,7 @@
 <script type="text/javascript">
 	$(function(){
 		$(".list").hover(function() {
-			$(this).addClass('list1').siblings('.list').removeClass('list1');
+			// $(this).addClass('list1').siblings('.list').removeClass('list1');
 			var i=$(this).attr('i');
 			$("#img"+i).addClass('img1').siblings('.img').removeClass('img1');
 		}, function() {
@@ -75,21 +78,36 @@ img{
 .list{
 	background-color: #ffffff;
 	height: 70px;
-	width: 253px;
+	width: 255px;
 	display: inline-block;
 	float: left;
-	border-left: 1px solid #ccc;
-	border-right: 1px solid #ccc;
+	/*border-left: 1px solid #ccc;*/
+	/*border-right: 1px solid #ccc;*/
 	padding: 5px;
 	/*border-radius: 3px;*/
-	margin-right: 1px;
+	/*margin-right: 1px;*/
 }
 /*.list:hover{
 	border: 1px solid #bc0000;
 }*/
 .list1{
-	background-color: #cccccc;
+	background-color: <?php echo $ys[0] ?>;
 	/*border: 1px solid #bc0000;*/
+	/*background-image: url(img/1.jpg);*/
+	/*background-repeat: repeat;*/
+	/*background-position: 0px 0px;*/
+}
+.list2{
+	background-color: <?php echo $ys[1] ?>;
+}
+.list3{
+	background-color: <?php echo $ys[2] ?>;
+}
+.list4{
+	background-color: <?php echo $ys[3] ?>;
+}
+.list5{
+	background-color: <?php echo $ys[4] ?>;
 }
 .list span{
 	display: block;
@@ -99,7 +117,7 @@ img{
 	margin: 5px 0px 5px 0px;
 }
 .list .bt a{
-	color: #000;
+	color: #fff;
 }
 .list .jg{
 	font-size: 28px;
@@ -123,10 +141,10 @@ img{
 	<div id="down">
 		<ul>
 			<li class="list list1" i="1"><span class="bt"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DnaH%252FCrshUsEjmraEDZVrLofrhNijlTODokW4bPA0wMWLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HyODyAjanCszm3abJM7sDg2L3Kj9Edr7oKJjU1rGI%252FCdg%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28024044">移动电源</a></span><span class="jg">¥ 49.9</span></li>
-			<li class="list" i="2"><span class="bt"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185">小米手机4</a></span><span class="jg">¥ 2098</span></li>
-			<li class="list" i="3"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
-			<li class="list" i="4"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
-			<li class="list" i="5"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
+			<li class="list list2" i="2"><span class="bt"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185">小米手机4</a></span><span class="jg">¥ 2098</span></li>
+			<li class="list list3" i="3"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
+			<li class="list list4" i="4"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
+			<li class="list list5" i="5"><span class="bt"><a target="_blank" href="#"></a></span><span class="jg">广告位</span></li>
 		</ul>
 	</div>
 </div>
