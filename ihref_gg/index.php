52a20758e0b8d088c06d6afb5d6b9ebbf8027c49
suyuwen1 <suyuwen1@126.com>
2014-11-28 21:09:48 +0800

    2014.11.28

--- ihref_gg/index.php
+++ ihref_gg/index.php
@@ -6,17 +6,17 @@
 <meta charset="utf-8">
 <meta name="url" content="http://www.ihref.com/ihref_gg/">
 <title>爱分享广告</title>
-<script src="js/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
 <!-- <link href="css/ihref_gg.css" type="text/css" rel="stylesheet"> -->
 <script type="text/javascript">
 	$(function(){
-		$(".list").hover(function() {
-			// $(this).addClass('list1').siblings('.list').removeClass('list1');
+		$(".list").mouseover(function(event) {
 			var i=$(this).attr('i');
 			$("#img"+i).addClass('img1').siblings('.img').removeClass('img1');
-		}, function() {
-
 		});
+		for (var i = 2; i <= $(".img").length; i++) {
+			$("#img"+i+" a img").attr('src', 'img/'+i+'.jpg');
+		};
 	})
 </script>
 <style type="text/css">
@@ -132,10 +132,10 @@ img{
 	<div id="top">
 		<ul>
 			<li class="img img1" id="img1"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DnaH%252FCrshUsEjmraEDZVrLofrhNijlTODokW4bPA0wMWLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HyODyAjanCszm3abJM7sDg2L3Kj9Edr7oKJjU1rGI%252FCdg%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28024044"><img src="img/1.jpg" alt="移动电源"></a></li>
-			<li class="img" id="img2"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185"><img src="img/2.jpg" alt="小米手机4"></a></li>
-			<li class="img" id="img3"><a target="_blank" href="#"><img src="img/3.jpg" alt=""></a></li>
-			<li class="img" id="img4"><a target="_blank" href="#"><img src="img/3.jpg" alt=""></a></li>
-			<li class="img" id="img5"><a target="_blank" href="#"><img src="img/3.jpg" alt=""></a></li>
+			<li class="img" id="img2"><a target="_blank" href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DfwrzGw19Pkq6k0Or%252B%252BH4tMBwlXCfbg2ZsmieqIX6sfqLltG5xFicOdXrTUTgh9sMDPIwxrc30rigOQv3H5R7StZ0c10rey9RLHhnGHvY3HwOvWWTr%252Bpe7m3abJM7sDg23aBtcmGUhOUT%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_33209569_8104433_28016185"><img alt="小米手机4"></a></li>
+			<li class="img" id="img3"><a target="_blank" href="#"><img alt=""></a></li>
+			<li class="img" id="img4"><a target="_blank" href="#"><img alt=""></a></li>
+			<li class="img" id="img5"><a target="_blank" href="#"><img alt=""></a></li>
 		</ul>
 	</div>
 	<div id="down">
